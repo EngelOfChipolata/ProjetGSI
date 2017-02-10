23c5841fab24be50980296ea91982ed917b9b3ef
buisangu <buisanguilhem@gmail.com>
2017-02-10 17:25:33 +0100

    Adds Windows Manager stuff

---
+++ HighFiProtos/AFleurDeuxPots802/src/afleurdeuxpots802/module/IFleur2PotsPage.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package afleurdeuxpots802.module;
+
+/**
+ *
+ * @author buisangu
+ */
+public abstract class AFleur2PotsPage {
+    public void ShowWindow
+}
